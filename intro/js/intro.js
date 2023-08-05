be53e4f15cba5dc6b3aa8dfed92e2e5f58d9e2f4
Purple-Code-sh <purple.code.sh@gmail.com>
2023-08-05 16:44:43 -0600

    reduce use

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -11,3 +11,10 @@ function operation(fn,a,b){
 }
 
 operation(sum,3,8);
+
+const someArray = [1,2,3,4,8];
+const total = someArray.reduce((def,actual)=>{
+    return def + actual
+},0)
+console.log("someArray total:", total);
+
